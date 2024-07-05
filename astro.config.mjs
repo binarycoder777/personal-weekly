9c42a6593efa5adc63af3357909fa7257eda90ca
atao <87337334@qq.com>
2024-07-05 14:28:32 +0800

    update style

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,26 +3,39 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	favicon: '/favicon.ico',
 	site: 'https://github.com/binarycoder777/binarycoder777.github.io',
+	integrations: [starlight({ title: 'Site with sitemap' })],
 	integrations: [
 		starlight({
-			title: 'BinaryCoder777',
+			customCss: [
+				// 你的自定义 CSS 文件的相对路径
+				'./src/styles/custom.css',
+				'@fontsource-variable/dancing-script',
+			],
+			title: {
+				'zh-CN': 'BinaryCoder777的周刊',
+				en: 'BinaryCoder777 Weekly',
+			},
+			logo: {
+				src: './src/assets/favicon.webp'
+			},
+			// 为此网站设置英语为默认语言。
+			defaultLocale: 'zh-cn',
+			locales: {
+				// 英文文档在 `src/content/docs/en/` 中。
+				en: {
+					label: 'English',
+				},
+				// 简体中文文档在 `src/content/docs/zh-cn/` 中。
+				'zh-cn': {
+					label: '简体中文',
+					lang: 'zh-CN',
+				},
+			},
 			social: {
 				github: 'https://github.com/binarycoder777',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
+			}
 		}),
 	],
 });
