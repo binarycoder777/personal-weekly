14c7914aff66eed7247482fe4a07313fb72f1b9d
weitao777 <87337334@qq.com>
2024-08-01 20:06:01 +0800

    Tuesday

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   favicon: '/favicon.ico',
-  site: 'https://github.com/binarycoder777',
+  site: 'https://binarycoder777.github.io',
   base: '/personal-weekly',
   integrations: [starlight({
     customCss: [
